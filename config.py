6515c1bea3414e508c781afa84392127af57667f
fzhou <felix990302@yahoo.ca>
2018-07-14 03:12:36 -0400

    changed env var according to Heroku standards

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-will-be-changed'  # TODO: changed secret key
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class ProductionConfig(Config):
