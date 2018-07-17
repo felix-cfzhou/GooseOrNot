868228a809eefb9a959b861450d2d5c09c1b092d
fzhou <felix990302@yahoo.ca>
2018-07-17 19:09:36 -0400

    made column for image file_name not nullable

--- server/models/image.py
+++ server/models/image.py
@@ -4,8 +4,8 @@ from server.database import db
 class Image(db.Model):
     __tablename__ = 'images'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    file_name = db.Column(db.String())
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    file_name = db.Column(db.String(), nullable=False)
 
     def __init__(self, file_name):
         self.file_name = file_name
