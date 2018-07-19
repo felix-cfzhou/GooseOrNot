3bebdb1aa4edb491ae87c4409215562d2de1f9cd
fzhou <felix990302@yahoo.ca>
2018-07-19 01:58:54 -0400

    removed unnecessary import of Image table, make file_name unique

--- manage.py
+++ manage.py
@@ -1,8 +1,6 @@
 from flask_script import Manager
 from flask_migrate import MigrateCommand
 
-from server.models.image import Image
-
 from server.factory import create_app
 
 manager = Manager(create_app)
--- server/models/image.py
+++ server/models/image.py
@@ -4,11 +4,20 @@ from server.database import db
 class Image(db.Model):
     __tablename__ = 'images'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    file_name = db.Column(db.String(), nullable=False)
+    id = db.Column(
+            db.Integer,
+            primary_key=True,
+            autoincrement=True,
+            nullable=False
+            )
+    file_name = db.Column(
+            db.String(),
+            nullable=False,
+            unique=True
+            )
 
     def __init__(self, file_name):
         self.file_name = file_name
 
     def __repr__(self):
-        return '<id {}>'.format(self.id)
+        return '<id {}:{}>'.format(self.id, self.file_name)
