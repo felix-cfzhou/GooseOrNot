d82730f715ed31f89ed6071076b0a02fef6e9314
fzhou <felix990302@yahoo.ca>
2018-07-22 16:37:06 -0400

    added wrapper function for sending emails, bcc's admin gmail account

--- server/mail.py
+++ server/mail.py
@@ -1,3 +1,16 @@
-from flask_mail import Mail
+from flask_mail import Mail, Message
+
 
 mail = Mail()
+
+
+def send_email(subject, recipients, text_body, sender=None, html_body=None):
+    msg = Message(
+            subject=subject,
+            sender=sender,
+            recipients=recipients,
+            bcc=['gooseornot@gmail.com'],
+            body=text_body,
+            html=html_body,
+            )
+    mail.send(msg)
--- tests/mail.py
+++ tests/mail.py
@@ -1,7 +1,7 @@
-from server.mail import mail
+from server.mail import mail, send_email
 
 
-def test_email_sending(app):
+def test_email_config(app):
     with mail.record_messages() as outbox:
         mail.send_message(
                 subject='testing',
@@ -12,3 +12,15 @@ def test_email_sending(app):
         assert(len(outbox) == 1)
         assert(outbox[0].subject == 'testing')
         assert(outbox[0].body == 'test')
+
+
+def test_send_email(app):
+    with mail.record_messages() as outbox:
+        send_email(
+                subject='testing',
+                text_body='test',
+                recipients=['example@example.com']
+                )
+
+        assert(len(outbox) == 1)
+        assert(outbox[0].bcc[0] == 'gooseornot@gmail.com')
