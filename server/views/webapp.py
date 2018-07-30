5ce0b4f55c848eafb5fe60a40388ce17042a76e9
fzhou <felix990302@yahoo.ca>
2018-07-29 23:50:01 -0400

    webapp url default to serve react file then client side nav takes over

--- server/views/webapp.py
+++ server/views/webapp.py
@@ -5,7 +5,8 @@ from flask_login import login_required
 webapp = Blueprint('webapp', __name__, url_prefix='/webapp', template_folder='templates')
 
 
-@webapp.route('/', methods=['GET', 'POST'])
+@webapp.route('/', defaults={'path': ''}, methods=['GET', 'POST'])
+@webapp.route('/<path:path>', methods=['GET', 'POST'])
 @login_required
-def index():
+def index(path):
     return render_template('index.html')
