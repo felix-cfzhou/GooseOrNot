ee0b140c2b109ef598f6c386a79cdb7bd9383f0f
fzhou <felix990302@yahoo.ca>
2018-08-03 21:37:35 -0400

    added restful login endpoints

---
+++ server/api/__init__.py
@@ -0,0 +1,49 @@
+from flask import Blueprint
+from flask_restful import Api, Resource, reqparse
+from flask_login import login_user, logout_user, current_user, login_required
+
+from server.models.user import User
+
+
+restful_home = Blueprint('api_home', __name__)
+restful_home_api = Api(restful_home, prefix='/api')
+
+
+class Login(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('username', type=str, required=True)
+    parser.add_argument('password', type=str, required=True)
+
+    def post(self):
+        if current_user.is_authenticated:
+            return {}, 204
+
+        args = self.parser.parse_args()
+        username = args['username']
+        password = args['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if user is None or not user.check_password(password):
+            json = dict(
+                    username="invalid username or password",
+                    password="invalid username or password"
+                    )
+            return json, 400
+
+        login_user(user)
+        return {}, 204
+
+
+class Logout(Resource):
+
+    @login_required
+    def post(self):
+        logout_user()
+
+        return {}, 204
+
+
+restful_home_api.add_resource(Login, '/login')
+restful_home_api.add_resource(Logout, '/logout')
