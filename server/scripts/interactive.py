c1b9ee99f3a0fa9f65f82edd5a0fa21bac4c7df0
fzhou <felix990302@yahoo.ca>
2018-08-03 21:41:22 -0400

    improved interactive script

--- server/scripts/interactive.py
+++ server/scripts/interactive.py
@@ -1,7 +1,16 @@
 from server.factory import create_app
 from server.database import db
 
+from server.models.user import User
+
 
 app = create_app()
 app.app_context().push()
-session = db.session
+
+connection = db.engine.connect()
+transaction = connection.begin()
+
+options = dict(bind=connection, binds={})
+session = db.create_scoped_session(options=options)
+
+db.session = session
